cc5dd8ddfc45f2fe7c51c7f209db6beea9a728d9
Mitchell Warren <liquidvisual@gmail.com>
2014-07-03 11:32:07 +1000

    First commit

---
+++ .tmp/scripts/main.js
@@ -0,0 +1,167 @@
+/*	@group Info
+
+	MAIN SCRIPTS - Last updated: 23-04-14
+*/
+
+//-----------------------------------------------------------------
+// IE Caution Message
+//-----------------------------------------------------------------
+
+$('.lv-alert .close-btn').click(function(){ $(this).parent().slideUp();});
+
+//-----------------------------------------------------------------
+// Topbar
+//-----------------------------------------------------------------
+
+$('#lv-topbar').lvTopBar(
+{
+	search: true,
+	searchAction: '/',
+	searchMethod: 'get',
+	searchName: 'q',
+	searchSelector: '.lv-search-icon',
+	searchText: 'Search',
+	clickDefault: 'mouseover'
+});
+
+//-----------------------------------------------------------------
+// Document Ready
+//-----------------------------------------------------------------
+
+$(document).ready(function() {
+
+	NProgress.start(); // Start preloader bar
+	setupHero(); // Setup Banner
+
+	// Magnific popup lightbox
+	// $('.popup-youtube').magnificPopup({
+ //        disableOn: 700,
+ //        type: 'iframe',
+ //        mainClass: 'mfp-fade',
+ //        removalDelay: 160,
+ //        preloader: false,
+ //        fixedContentPos: false
+ //      });
+});
+
+//-----------------------------------------------------------------
+// setupHero
+//-----------------------------------------------------------------
+
+function setupHero(){
+
+	var isMobile = $(window).width() < 768;
+	var heroCount = $(".lv-hero").length;
+	var heroModule = $('.lv-hero-wrapper');
+
+	//==================================================
+	// Preload image
+	//==================================================
+
+		$('.lv-hero').each(function(index){
+
+			var target = $(this);
+			var imgPath = target.attr('data-background-image');
+
+			// Preload the first hero
+
+			if (index == 0) {
+				$('<img src="'+imgPath+'">').bind('load', function(e) {
+					heroModule.addClass('is-loaded');
+					launchHero(heroModule);
+					NProgress.done();
+					target.hide().fadeIn(1000); // must go last
+				});
+			}
+
+			// Add the background images, except on >0 on mobile
+			// Mobile won't load additional images
+
+			else if (isMobile && index > 0) return;
+
+			target.css({"background-image": "url("+imgPath+")"});
+
+		});
+
+	//==================================================
+	// launchHero
+	//==================================================
+
+	function launchHero(target){
+
+		if (heroCount > 1) {
+
+			bxSlider = $('#lv-hero-carousel').bxSlider(
+			{
+				auto: (isMobile ? false : true),
+				mode: 'horizontal',
+				adaptiveHeight: true,
+				responsive: true,
+				touchEnabled: false,
+				pause: 8000, // Slow timer
+				slideMargin: 0,
+				minSlides: 1,
+				controls: false,
+				infiniteLoop: true,
+				pager: (isMobile ? false : true),
+				pagerSelector: '.lv-hero-carousel-bullets',
+				onSliderLoad:  function(){  $('.lv-hero-caption').show().addClass('fadeInLeft');},
+				onSlideBefore: function(){  $('.lv-hero-caption').hide().removeClass('fadeInLeft');},
+				onSlideAfter:  function(){  $('.lv-hero-caption').show().addClass('fadeInLeft');}
+			});
+
+			// Switch off slider for mobile (better user experience)
+
+			$(window).resize(function() {
+				var isMobile = $(window).width() < 768;
+				if (isMobile) bxSlider.stopAuto();
+			});
+
+			$(window).resize();
+
+		} else {
+
+		//	$('.lv-hero-caption').delay(10000).addClass('fadeInLeft');
+
+			$('.lv-hero-caption').css("visibility", "hidden").delay(500).queue(function(next){
+			    $(this).css("visibility", "visible").addClass('fadeInLeft');
+			    next();
+			});
+		}
+	}
+	//if (bxCarousel) bxCarousel.destroySlider();
+}
+
+//-----------------------------------------------------------------
+// +++ HELPERS +++
+//-----------------------------------------------------------------
+
+function trace(command){console.log(command+" ("+typeof(command)+")");}
+
+//==================================================
+// Search Form to send by hitting ENTER
+//==================================================
+
+$("#search-form").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        $("#search-form").submit();
+    }
+});
+
+//==================================================
+// Developer: COMMAND+S for screen width
+//==================================================
+
+$(document).keypress(function(event) {
+    if (event.which == 115 && (event.ctrlKey||event.metaKey)|| (event.which == 19)) {
+        event.preventDefault();
+        alert("(w) "+$(window).width()+" (h) "+$(window).height());
+        return false;
+    }
+    return true;
+});
+
+//==================================================
+//
+//==================================================
---
+++ Gruntfile.js
@@ -0,0 +1,181 @@
+module.exports = function (grunt) {
+
+   // load time-grunt and all grunt plugins found in the package.json
+   require('time-grunt')(grunt);
+   require('jit-grunt')(grunt);
+
+   grunt.initConfig({
+      //-----------------------------------------------------
+      // Configure Paths
+      //-----------------------------------------------------
+      config: {
+         app: 'client',
+         dist: 'dist',
+         port: '9999',
+         takanaOn: true, // see notes in 'Watch'
+         git: 'git@github.com:liquidvisual/ewide.com.au.git'
+      },
+      //-----------------------------------------------------
+      // TAKANA - live Sass refreshing
+      //
+      // Sometimes Takana screws up with Foundation's 'functions.scss'.
+      // To fix, remove '/_scss' below. Run grunt takana, then quit..
+      // add '/_scss' back onto it and re-run. Don't ask.
+      //-----------------------------------------------------
+      takana: {
+         options: {
+            path: '<%= config.app %>/_scss'
+         }
+      },
+      //-----------------------------------------------------
+      // JEKYLL
+      //-----------------------------------------------------
+      shell: {
+        jekyllServe: {
+            command: "jekyll build --source <%= config.app %>  --destination .tmp"
+         },
+         jekyllBuild: {
+            command: "jekyll build --source <%= config.app %>  --destination <%= config.dist %> --config <%= config.app %>/_config.deploy.yml"
+         }
+      },
+      //-----------------------------------------------------
+      // BUILD CONTROL (GIT)
+      //-----------------------------------------------------
+      buildcontrol: {
+        options: {
+          commit: true,
+          connectCommits: false,
+          push: true,
+          message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+        },
+        master: {
+          options: {
+            dir: './',
+            remote: '<%= config.git %>',
+            branch: 'master'
+          }
+        },
+        pages: {
+          options: {
+            dir: '<%= config.dist %>',
+            remote: '<%= config.git %>',
+            branch: 'gh-pages'
+          }
+        },
+        local: {
+          options: {
+            remote: '../',
+            branch: 'build'
+          }
+        }
+      },
+      //-----------------------------------------------------
+      // SASS - Compiles sass only, leaves .css alone
+      //-----------------------------------------------------
+      sass: {
+        options: {
+            //outputStyle: 'compressed'
+        },
+        temp: {
+            files: {
+                '.tmp/css/main.css' : '<%= config.app %>/_scss/main.scss',
+                '.tmp/css/liquidvisual.css' : '<%= config.app %>/_scss/liquidvisual/liquidvisual.scss',
+                '.tmp/css/foundation.css' : '<%= config.app %>/_scss/foundation/foundation.scss'
+            }
+          }
+      },
+      //-----------------------------------------------------
+      // A. CONNECT
+      //-----------------------------------------------------
+      connect: {
+         options: {
+            port: '<%= config.port %>',
+            livereload: 35729,
+            hostname: '0.0.0.0'
+         },
+         livereload: {
+            options: {
+               open: true,
+               base: ['.tmp', '<%= config.app %>']
+            }
+         },
+         dist: {
+            options: {
+               open: true,
+               base: '<%= config.dist %>',
+               livereload: false
+            }
+         }
+      },
+      //-----------------------------------------------------
+      // B. WATCH
+      //-----------------------------------------------------
+      watch: {
+
+        options: {
+          livereload: true,
+          spawn: true
+        },
+
+         sass: {
+            files: ['<%= config.app %>/_scss/**/*.{scss,sass}'],
+            tasks: ['sass:temp'],
+            options : {
+               livereload: '<%= config.takanaOn %>' // SWITCH true to live inject but BREAK Takana
+            }
+         },
+
+         css: {
+            files: ['.tmp/css/**/*.css'],
+            tasks: [],
+            reload: true
+         },
+
+         // If any of these files change:
+         // 1. Run Jekyll Build
+         // 2. Compile Sass and drop into _site
+
+         livereload: {
+            options: {
+               livereload: '<%= connect.options.livereload %>'
+            },
+            // files: ['<%= config.app %>/index.html'],
+            files: [
+                '<%= config.app %>/**/*.html',
+                '<%= config.app %>/**/*.yml',
+                '<%= config.app %>/**/*.md',
+                '<%= config.app %>/scripts/**/*.js',
+                '<%= config.app %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}'
+                //'!<%= config.app %>/**/_s/**'
+            ],
+            tasks: ['shell:jekyllServe', 'sass:temp']
+         }
+      }
+   //-- end initConfig
+   });
+   //-----------------------------------------------------
+   // Register Tasks
+   //-----------------------------------------------------
+   grunt.registerTask('serve', [
+      'shell:jekyllServe',
+      'sass:temp',
+      'connect:livereload',
+      'watch'
+   ]);
+
+   // Build only
+   grunt.registerTask('deploy', [
+      'shell:jekyllBuild',
+      'sass:dist',
+      'buildcontrol:master',
+      'buildcontrol:pages'
+   ]);
+
+   // Build only
+   grunt.registerTask('default', [
+      'shell:jekyllBuild',
+      'sass:dist'
+   ]);
+
+//-- end module.exports
+};
